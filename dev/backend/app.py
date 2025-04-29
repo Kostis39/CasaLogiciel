774236a0b3b48f68ee2926eb0f74bc8d59f56c59
Matteo Vert <Matteo.VERT@etu.uca.fr>
2025-04-29 20:35:46 +0200

    premiers tests

--- dev/backend/app.py
+++ dev/backend/app.py
@@ -1,5 +1,16 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_restful import Resource,Api
 
 app = Flask(__name__)
 CORS(app)
+api = Api(app)
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'First Json':'ok'}
+
+api.add_resource(HelloWorld, '/')
+
+if __name__=='__main__':
+    app.run(debug=True)
